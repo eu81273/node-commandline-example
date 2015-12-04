f2a7b5b902f3766d8c64d1c3544b98c6e6755ce1
AHN JAE-HA <eu81273@gmail.com>
2015-12-04 16:48:09 +0900

    Add coloring terminal output

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var co = require('co');
 var coPrompt = require('co-prompt');
 var commander = require('commander');
+var chalk = require('chalk');
 
 commander
 	.arguments('<file>')
@@ -12,7 +13,9 @@ commander
 			var username = yield coPrompt('username: ');
 			var password = yield coPrompt.password('password: ');
 
-			console.log('사용자는 %s\n패스워드는 %s\n파일은 %s', username, password, file);
+			console.log(chalk.green.bold('사용자: ') + username);
+			console.log(chalk.blue.bold('패스워드: ') + password);
+			console.log(chalk.black.bgWhite.bold('파일: ') + file);
 			process.exit(0);
 		});
 	})
