72f4d068148652e037fb38fc691de78ee5870b2a
AHN JAE-HA <eu81273@gmail.com>
2015-12-04 17:57:00 +0900

    Add progress bar

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var co = require('co');
 var coPrompt = require('co-prompt');
 var commander = require('commander');
 var chalk = require('chalk');
+var ProgressBar = require('progress');
 
 commander
 	.arguments('<file>')
@@ -16,7 +17,22 @@ commander
 			console.log(chalk.green.bold('사용자: ') + username);
 			console.log(chalk.blue.bold('패스워드: ') + password);
 			console.log(chalk.black.bgWhite.bold('파일: ') + file);
-			process.exit(0);
+
+			var bar = new ProgressBar('작업상황 [:bar] :percent :etas', {
+				total: 10,
+				width: 20,
+				complete: '>',
+				incomplete: '_'
+			});
+
+			var timer = setInterval(function () {
+				bar.tick();
+				if (bar.complete) {
+					console.log(chalk.cyan('\n작업 완료'));
+					clearInterval(timer);
+					process.exit(0);
+				}
+			}, 500);
 		});
 	})
 	.parse(process.argv);
