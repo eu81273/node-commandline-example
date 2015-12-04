c02ffe7ddd58bddb4bfc8c9974252d79436e5d45
AHN JAE-HA <eu81273@gmail.com>
2015-12-04 16:36:11 +0900

    Add prompting for user input

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+var co = require('co');
+var coPrompt = require('co-prompt');
 var commander = require('commander');
 
 commander
@@ -6,6 +8,11 @@ commander
 	.option('-u, --username <username>', '사용자')
 	.option('-p, --password <password>', '패스워드')
 	.action(function (file) {
-		console.log('사용자는 %s\n패스워드는 %s\n파일은 %s', options.username, options.password, file);
+		co(function* () {
+			var username = yield coPrompt('username: ');
+			var password = yield coPrompt.password('password: ');
+
+			console.log('사용자는 %s\n패스워드는 %s\n파일은 %s', username, password, file);
+		});
 	})
 	.parse(process.argv);
